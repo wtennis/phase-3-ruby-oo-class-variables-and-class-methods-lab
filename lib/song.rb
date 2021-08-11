5fb393a736776656dcc364b8c4c4c33cb88610f1
Whiting <whitinghtennis@gmail.com>
2021-08-11 09:39:25 -0700

    commit tree 9847c84377053fd163c88652859dd6736813d0e2

---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@artist_count = {}
+
+    def self.artist_count
+        @@artist_count
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genre_count
+    end
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+
+        if @@genre_count[genre]
+            @@genre_count[genre] += 1
+            else 
+            @@genre_count[genre] = 1
+            end
+
+        if @@artist_count[artist]
+            @@artist_count[artist] += 1
+            else 
+            @@artist_count[artist] = 1
+            end
+    end
+
+end
+
+thriller = Song.new('Thriller', 'Michael Jackson', 'Pop')
+beat_it = Song.new('Beat It', 'Michael Jackson', 'Pop')
+
+# ruby './lib/song.rb'
